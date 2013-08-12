d2e74a1c5f54866e8d97a3eb923058f8c5ffbc42
tschaefer <github@blackox.org>
2013-08-12 07:11:48 +0200

    reworked models, admin classes

--- stechuhr/admin.py
+++ stechuhr/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from stechuhr.models import UserProfile, WorkDay, Meeting
+from stechuhr.models import UserProfile, UserSettings, WorkDay
 
 
 class UserProfileInline(admin.StackedInline):
@@ -17,25 +17,25 @@ class UserAdmin(UserAdmin):
 	inlines = (UserProfileInline, )
 
 
-class MeetingAdmin(admin.ModelAdmin):
-	list_display = ('id', 'date', 'user', 'title', 'kind', 'begin', 'end')
-	search_fields = ('user', 'title', )
-	date_hierachy = 'date'
-	list_filter = ('date', )
-	list_display_links = ('title', 'id', )
-
+class UserSettingsAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user', 'valid_from_date', 'valid_to_date', 'company',
+			'hours_per_week', 'pause_per_day', 'leave_days_per_year', )
+	search_fields = ('user', )
+	list_filter = ('valid_from_date', 'valid_to_date', )
+	list_display_links = ('id', 'user', 'valid_from_date', )
 
 class WorkDayAdmin(admin.ModelAdmin):
-	list_display = ('id', 'date', 'user', 'kind', 'begin', 'end', )
+	list_display = ('id', 'date', 'user', 'kind_of_workday', 'start_time',
+			'end_time', )
 	search_fields = ('user', )
-	date_hierachy = 'date'
-	list_filter = ('date', )
+	date_hierachy = ('date', )
+	list_filter = ('date', 'kind_of_workday', )
 	list_display_links = ('user', 'id', )
 
 
 admin.site.unregister(User)
+admin.site.register(UserSettings, UserSettingsAdmin)
 admin.site.register(User, UserAdmin)
 admin.site.register(WorkDay, WorkDayAdmin)
-admin.site.register(Meeting, MeetingAdmin)
 
 # vim: set ft=python ts=4 sw=4 :
--- stechuhr/models.py
+++ stechuhr/models.py
@@ -3,27 +3,55 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+def validate_workday_hours_per_week(hours):
+	if hours > 168:
+		raise ValidationError('Too many workday hours per week')
+
+def validate_pause_minutes_per_day(minutes):
+	if minutes > 1440:
+		raise ValidationError('Too many pause minutes per day')
+
+def validate_leave_days_per_year(days):
+	if days > 365:
+		raise ValidationError('Too many leave days per year')
+
+
+class UserSettings(models.Model):
+	class Meta:
+		ordering = ('-create_date', 'user', )
+		verbose_name_plural = 'User settings'
+
+	def __unicode__(self):
+		return '%s %s' % (self.user, self.company)
+
+	user = models.ForeignKey(User)
+	create_date = models.DateTimeField()
+	company = models.CharField(max_length=255, null=True, blank=True)
+	department = models.CharField(max_length=255, null=True, blank=True)
+	position = models.CharField(max_length=255, null=True, blank=True)
+	valid_from_date = models.DateField(auto_now_add=False, null=True,
+			blank=True)
+	valid_to_date = models.DateField(auto_now_add=False, null=True, blank=True)
+	hours_per_week = models.PositiveSmallIntegerField(null=True, blank=True,
+			validators=[validate_workday_hours_per_week])
+	pause_minutes_per_day = models.PositiveSmallIntegerField(null=True, blank=True,
+			validators=[validate_pause_minutes_per_day])
+	leave_days_per_year = models.PositiveSmallIntegerField(null=True,
+			blank=True, validators=[validate_leave_days_per_year])
 
 
 class UserProfile(models.Model):
 	class Meta:
-		ordering = ['user']
+		ordering = ('user',)
 
 	def __unicode__(self):
 		return '%s' % self.user
 
 	user = models.OneToOneField(User)
-	company = models.CharField(max_length=255, null=True, blank=True)
-	department = models.CharField(max_length=255, null=True, blank=True)
-	position = models.CharField(max_length=255, null=True, blank=True)
-	place = models.CharField(max_length=255, null=True, blank=True)
-	#workdays_per_week = models.PositiveSmallIntegerField(null=True, blank=True)
-	hours_per_week = models.PositiveSmallIntegerField(null=True, blank=True)
-	pause_per_day = models.TimeField(null=True, blank=True)
-	core_working_time_begin = models.TimeField(null=True, blank=True)
-	core_working_time_end = models.TimeField(null=True, blank=True)
-	days_of_holiday_per_year = models.PositiveSmallIntegerField(null=True,
-			blank=True)
+	settings = models.ManyToManyField(UserSettings, null=True, blank=True)
 
 	def create_user_profile(sender, instance, created, **kwargs):
 		if created:
@@ -32,66 +60,39 @@ class UserProfile(models.Model):
 	post_save.connect(create_user_profile, sender=user)
 
 
-class Meeting(models.Model):
-	class Meta:
-		ordering = ['-date', 'user']
-
-	def __unicode__(self):
-		title = (self.title[:25] + '...') if len(self.title) > 25 else self.title
-		return '%s' % title
-
-	MEETING_CHOICES = (
-			('ONE', 'One on one interview'),
-			('TEAM', 'Team meeting'),
-			('REG', 'Regular meeting'),
-			('CONF', 'Conference'),
-			('DISC', 'Discussion'),
-			('PHONE', 'Telephone Conference'),
-			('VIDEO', 'Video Conference'),
-		)
-
-	user = models.ForeignKey(UserProfile)
-	date = models.DateField(auto_now_add=False)
-	title = models.CharField(max_length=255)
-	kind = models.CharField(max_length=255, choices=MEETING_CHOICES,
-			default='TEAM')
-	begin = models.TimeField(auto_now_add=False)
-	end = models.TimeField(auto_now_add=False)
-
-
 class WorkDay(models.Model):
 	class Meta:
-		ordering = ['-date', 'user']
+		ordering = ('-date', 'user',)
 
 	WORKDAY_CHOICES = (
-			('DR', u'Daily routine'),
-			('UFW', u'Unfitness for work'),
-			('T', u'Training'),
-			('S', u'Seminar'),
-			('BT', u'Business Trip'),
-			('WT', u'Works outing')
+			(u'Daily routine', u'Daily routine'),
+			(u'Training', u'Training'),
+			(u'Seminar', u'Seminar'),
+			(u'Business Trip', u'Business Trip'),
+			(u'Unfitness for work', u'Unfitness for work'),
+			(u'Vacation', u'Vacation'),
+			(u'Flextime compensation', u'Flextime compensation'),
 		)
 
 	def __unicode__(self):
 		return '%s %s' % (self.user, self.date.strftime('%y/%m/%d'))
 
-	user = models.ForeignKey(UserProfile)
-	date = models.DateField(auto_now_add=False, unique=True)
-	kind = models.CharField(max_length=255, choices=WORKDAY_CHOICES,
-			default='DR')
-	begin = models.TimeField(auto_now_add=False)
-	end = models.TimeField(auto_now_add=False, null=True,
+	user = models.ForeignKey(User)
+	created_date = models.DateTimeField()
+	date = models.DateField(auto_now_add=False)
+	kind_of_workday = models.CharField(max_length=255, choices=WORKDAY_CHOICES,
+			default='Daily routine')
+	start_time = models.TimeField(auto_now_add=False)
+	end_time = models.TimeField(auto_now_add=False, null=True,
 			blank=True)
-	pause = models.TimeField(auto_now_add=False, null=True, blank=True)
-	place = models.CharField(max_length=255, null=True, blank=True)
-	meetings = models.ManyToManyField(Meeting, null=True, blank=True,
-			related_name='meetings')
-	description = models.TextField()
+	pause_minutes = models.TimeField(auto_now_add=False, null=True, blank=True)
+	workplace = models.CharField(max_length=255, null=True, blank=True)
+	report = models.TextField()
 
 	def get_duration(self):
-		if self.end is None:
+		if self.end_time is None:
 			return None
 		else:
-			return self.end - self.begin
+			return self.end_time - self.start_time
 
 # vim: set ft=python ts=4 sw=4 :
