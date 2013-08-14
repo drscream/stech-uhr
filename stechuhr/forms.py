5e35c5f49baa3cd0f5c4d5ea9ad447d117881084
tschaefer <github@blackox.org>
2013-08-14 14:45:51 +0200

    added form class for 'signin'

--- stechuhr/forms.py
+++ stechuhr/forms.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 from django import forms
 from django.contrib.auth.models import User
 
@@ -38,6 +40,7 @@ class UserWorkSettingsForm(forms.Form):
 		settings.pause_minutes_per_day = self.cleaned_data['pause_minutes_per_day']
 		settings.leave_days_per_year = self.cleaned_data['leave_days_per_year']
 		settings.save()
+		return settings
 
 	def modify(self):
 		settings = UserSettings.objects.get(pk=self.cleaned_data['pk'])
@@ -50,5 +53,47 @@ class UserWorkSettingsForm(forms.Form):
 		settings.pause_minutes_per_day = self.cleaned_data['pause_minutes_per_day']
 		settings.leave_days_per_year = self.cleaned_data['leave_days_per_year']
 		settings.save()
+		return settings
+
+class SigninForm(forms.Form):
+	username = forms.RegexField(max_length=30, regex=r'^[\w.@+-]+$')
+	first_name = forms.CharField(max_length=254)
+	last_name = forms.CharField(max_length=254)
+	email = forms.EmailField()
+	confirm_email = forms.EmailField()
+	password = forms.CharField(widget=forms.PasswordInput)
+	confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+	def clean_username(self):
+		username = self.cleaned_data["username"]
+		try:
+			User._default_manager.get(username=username)
+		except User.DoesNotExist:
+			return username
+		raise forms.ValidationError('duplicate_username')
+
+	def clean_confirm_email(self):
+		email = self.cleaned_data.get('email')
+		confirm_email = self.cleaned_data.get('confirm_email')
+		if email != confirm_email:
+			raise forms.ValidationError('email missmatch')
+		return confirm_email
+
+	def clean_confirm_password(self):
+		password = self.cleaned_data.get('password')
+		confirm_password = self.cleaned_data.get('confirm_password')
+		if password != confirm_password:
+			raise forms.ValidationError('password missmatch')
+		return confirm_password
+
+	def save(self):
+		user = User()
+		user.username = self.cleaned_data['username']
+		user.first_name = self.cleaned_data['first_name']
+		user.last_name = self.cleaned_data['last_name']
+		user.email = self.cleaned_data['email']
+		user.set_password(self.cleaned_data["password"])
+		user.save()
+		return user
 
 # vim: set ft=python ts=4 sw=4 :
