8cb1e507974e118eab3a7c505c30f7c2f667f44a
tschaefer <github@blackox.org>
2013-08-19 15:59:26 +0200

    added utils file

---
+++ stechuhr/utils.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+
+import datetime
+
+def isoweek_startdate(year, week):
+    startdate = datetime.datetime.strptime('%04d-%02d-1' % (year, week), '%Y-%W-%w')
+    if datetime.date(year, 1, 4).isoweekday() > 4:
+        startdate -= datetime.timedelta(days=7)
+    return startdate
+
+# vim: set ft=python ts=4 sw=4 :
