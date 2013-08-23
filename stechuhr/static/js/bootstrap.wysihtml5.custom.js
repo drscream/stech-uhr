f2250e71df878126d3782180f75c76fa0ae8375a
tschaefer <github@blackox.org>
2013-08-23 15:27:32 +0200

    add css/js files 'WYSIWYG editor'

---
+++ stechuhr/static/js/bootstrap.wysihtml5.custom.js
@@ -0,0 +1,11 @@
+$('#id_log').wysihtml5({
+	"font-styles": true,
+	"emphasis": true,
+	"lists": true,
+	"html": false,
+	"link": false,
+	"image": false,
+	"color": false,
+});
+
+/** vim: set ft=javascript ts=4 sw=4 : */
