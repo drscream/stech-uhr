72d6fdf44551c019af49c3505d810ed6fdf440ce
tschaefer <github@blackox.org>
2013-08-27 21:35:19 +0200

    removed tag filter 'leading_zero'; renamed 'timeuntil_with_timedelta'

--- stechuhr/templatetags/stechuhr-tags.py
+++ stechuhr/templatetags/stechuhr-tags.py
@@ -17,16 +17,12 @@ def append(value, arg):
 	else:
 		return ''
 
-@register.filter(name='leading_zero')
-def leading_zero(value):
-	return '%02d' % (int(value))
-
 @register.filter(name='md')
 def md(value):
 	return markdown.markdown(value)
 
-@register.filter(name='timedelta_until')
-def timedelta_until(tdelta):
+@register.filter(name='timeuntil_with_timedelta')
+def timeuntil_with_timedelta(tdelta):
 	if not isinstance(tdelta, datetime.timedelta):
 		raise TemplateSyntaxError("invalid timedelta object")
 	return timeuntil(datetime.datetime.now() + tdelta)
